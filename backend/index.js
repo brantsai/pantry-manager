7944b6ab1cb6941b3f069a508c5e9e50ca0857e6
unknown <bjt888@gmail.com>
2025-05-08 14:41:16 -0700

    minor adjustment

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,6 @@ const app = express();
 const PORT = 5000;
 
 const recommendRecipes = require('./utils/recommendRecipes')
-const pantry = require('./data/pantry.json')  // TEMPORARY PANTRY
 
 app.use(cors());
 app.use(express.json());
