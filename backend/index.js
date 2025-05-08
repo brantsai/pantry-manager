0dfb31868e20a50bf7d47c934abd1ae050558a25
unknown <bjt888@gmail.com>
2025-05-08 14:39:52 -0700

    added recommend route

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,39 @@ const cors = require('cors');
 const app = express();
 const PORT = 5000;
 
+const recommendRecipes = require('./utils/recommendRecipes')
+const pantry = require('./data/pantry.json')  // TEMPORARY PANTRY
+
 app.use(cors());
 app.use(express.json());
+
+/**
+ * ROUTES
+ */
+
+
+/**
+ * POST /recommend
+ * Accepts an array of pantry items and returns a sorted
+ * array of recipes with match percentage and missing 
+ * ingredients using recommendRecipe function.
+ */
+app.post('/recommend', (req, res) => {
+    try {
+        const pantryItems = req.body.pantry;
+
+        if (!Array.isArray(pantryItems)) {
+            return res.status(400).json({ error: "Missing or invalid pantry array" })
+        }
+
+        const recommendedRecipes = recommendRecipes(pantryItems);
+        res.status(200).json(recommendedRecipes);
+    }
+    catch (err) {
+        console.error('Error in /recommend', err);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
